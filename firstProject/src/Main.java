7e022bfd7bfdd113698a1842094932f87240a910
isaak <kimisaak0@gmail.com>
2022-04-22 18:37:48 +0900

    author name change test

--- firstProject/src/Main.java
+++ firstProject/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!!!!");
         System.out.println("isaak!!!!");
         System.out.println("sangmin!!!!22fgffffgdfg222");
+        System.out.println("author name change");
     }
 }
